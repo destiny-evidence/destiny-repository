ef4d0a693f5634cd56acea015b95380fc535ed28
Andrew Harvey <aharvey@covidence.org>
2025-02-18 09:44:40 +1100

    Improve docstrings and descriptions for import classes

--- app/models/import_batch.py
+++ app/models/import_batch.py
@@ -1,7 +1,7 @@
 """Models for import batches."""
 
 import uuid
-from enum import Enum, auto
+from enum import Enum
 from typing import TYPE_CHECKING, Optional
 
 from pydantic import HttpUrl
@@ -12,18 +12,38 @@ if TYPE_CHECKING:
 
 
 class ImportBatchStatus(str, Enum):
-    """Describes the status of an import batch."""
+    """
+    Describes the status of an import batch.
 
-    created = auto()
-    started = auto()
-    completed = auto()
-    cancelled = auto()
+    - `created`: Created, but no processing has started.
+    - `started`: Processing has started on the batch.
+    - `completed`: Processing has been completed.
+    - `cancelled`: Processing was cancelled by calling the API.
+    """
+
+    created = "created"
+    started = "started"
+    completed = "completed"
+    cancelled = "cancelled"
 
 
 class ImportBatch(SQLModel, table=True):
-    """The base class for import batches."""
+    """
+    The base class for import batches.
 
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    An import batch is a set of references imported together as part of a larger
+    import. They wrap a storage URL and track the status of the importing of the
+    file at that storage url.
+    """
+
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        primary_key=True,
+        description="""
+The identifier of the batch, which may be set by the processor or will be
+generated on creation.
+""",
+    )
     import_id: uuid.UUID | None = Field(default=None, foreign_key="importrecord.id")
     import_record: Optional["ImportRecord"] = Relationship(back_populates="batches")
     status: ImportBatchStatus = ImportBatchStatus.created
