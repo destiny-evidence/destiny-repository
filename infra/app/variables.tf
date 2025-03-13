4eb8fd145b1ba5372fafe84bb9ea89c96e00c8a2
Jack Walmisley <jack@covidence.org>
2025-03-13 13:30:01 +1300

    Marking admin password as sensitive

--- infra/app/variables.tf
+++ infra/app/variables.tf
@@ -12,6 +12,7 @@ variable "admin_login" {
 variable "admin_password" {
   type        = string
   description = "admin password for the app database"
+  sensitive   = true
 }
 
 variable "app_max_replicas" {
