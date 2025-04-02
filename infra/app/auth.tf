85051ca3216159b9b09c629a312ddf27686c3e48
Jack Walmisley <jack@covidence.org>
2025-04-02 17:11:38 +1300

    Refactored to use app registration resources
    Removes the need to have ignore lifecycle pieces in the application resource block

--- infra/app/auth.tf
+++ infra/app/auth.tf
@@ -1,57 +1,52 @@
 resource "random_uuid" "importer_role" {}
 
-# Active directory application for destiny repository
+# App registration for destiny repository
 # App roles to allow various functions (i.e. imports) should be added as app role resources here
-resource "azuread_application" "destiny_repository" {
+resource "azuread_application_registration" "destiny_repository" {
   display_name     = "Destiny Repository ${var.environment}"
-  owners           = [data.azuread_client_config.current.object_id]
   sign_in_audience = "AzureADMyOrg"
+}
 
-  app_role {
-    allowed_member_types = ["User", "Application"]
-    description          = "Importers can import"
-    display_name         = "Importers"
-    id                   = random_uuid.importer_role.id
-    value                = "import"
-    enabled              = true
-  }
-
-  lifecycle {
-    ignore_changes = [
-      identifier_uris,
-    ]
-  }
+resource "azuread_application_app_role" "importer" {
+  application_id       = azuread_application_registration.destiny_repository.id
+  allowed_member_types = ["User", "Application"]
+  description          = "Importers can import"
+  display_name         = "Importers"
+  role_id              = random_uuid.importer_role.result
+  value                = "import"
 }
 
 resource "azuread_service_principal" "destiny_repository" {
-  client_id                    = azuread_application.destiny_repository.client_id
+  client_id                    = azuread_application_registration.destiny_repository.client_id
   app_role_assignment_required = false
   owners                       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_application_identifier_uri" "this" {
-  application_id = azuread_application.destiny_repository.id
-  identifier_uri = "api://${azuread_application.destiny_repository.client_id}"
+  application_id = azuread_application_registration.destiny_repository.id
+  identifier_uri = "api://${azuread_application_registration.destiny_repository.client_id}"
 }
 
 # Create an application that we can use to authenticate with the Destiny Repository
-resource "azuread_application" "destiny_repository_auth" {
+resource "azuread_application_registration" "destiny_repository_auth" {
   display_name     = "Destiny Repository Auth ${var.environment}"
-  owners           = [data.azuread_client_config.current.object_id]
   sign_in_audience = "AzureADMyOrg"
+}
 
-  required_resource_access {
-    resource_app_id = azuread_application.destiny_repository.client_id
+resource "azuread_application_api_access" "destiny_repository_auth" {
+  application_id = azuread_application_registration.destiny_repository_auth.id
+  api_client_id  = azuread_application_registration.destiny_repository.client_id
 
-    resource_access {
-      id   = azuread_service_principal.destiny_repository.app_role_ids["import"]
-      type = "Role"
-    }
-  }
+  role_ids = [
+    azuread_application_app_role.importer.role_id,
+  ]
+
+  scope_ids = []
 }
 
 resource "azuread_application_redirect_uris" "local_redirect" {
-  application_id = azuread_application.destiny_repository_auth.id
+  # This is necessary to return the token to you if you're grabbing a token for local dev
+  application_id = azuread_application_registration.destiny_repository_auth.id
   type           = "PublicClient"
 
   redirect_uris = [
@@ -60,13 +55,13 @@ resource "azuread_application_redirect_uris" "local_redirect" {
 }
 
 resource "azuread_service_principal" "destiny_repository_auth" {
-  client_id                    = azuread_application.destiny_repository_auth.client_id
+  client_id                    = azuread_application_registration.destiny_repository_auth.client_id
   app_role_assignment_required = false
   owners                       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_app_role_assignment" "importer" {
-  app_role_id         = azuread_application.destiny_repository.app_role_ids["import"]
+  app_role_id         = azuread_application_app_role.importer.role_id
   principal_object_id = azuread_service_principal.destiny_repository_auth.object_id
   resource_object_id  = azuread_service_principal.destiny_repository.object_id
 }
