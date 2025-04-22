db615c1418ca403ddaa8cdef88208d3623e7f7c5
Jack Walmisley <jack@covidence.org>
2025-04-22 16:43:54 +1200

    Breaking out into read/write references scopes

--- infra/app/auth.tf
+++ infra/app/auth.tf
@@ -1,6 +1,8 @@
 resource "random_uuid" "importer_role" {}
 
-resource "random_uuid" "referencer_role" {}
+resource "random_uuid" "reference_reader_role" {}
+
+resource "random_uuid" "reference_writer_role" {}
 
 # App registration for destiny repository
 # App roles to allow various functions (i.e. imports) should be added as app role resources here
@@ -19,13 +21,22 @@ resource "azuread_application_app_role" "importer" {
   value                = "import"
 }
 
-resource "azuread_application_app_role" "referencer" {
+resource "azuread_application_app_role" "reference_reader" {
   application_id       = azuread_application_registration.destiny_repository.id
   allowed_member_types = ["User", "Application"]
-  description          = "Can view, add, and request enhancement on references"
-  display_name         = "referencer"
-  role_id              = random_uuid.referencer_role.result
-  value                = "referencer"
+  description          = "Can view references"
+  display_name         = "Reference Reader"
+  role_id              = random_uuid.reference_reader_role.result
+  value                = "reference.reader"
+}
+
+resource "azuread_application_app_role" "reference_writer" {
+  application_id       = azuread_application_registration.destiny_repository.id
+  allowed_member_types = ["User", "Application"]
+  description          = "Can create and enhance references"
+  display_name         = "Reference Writer"
+  role_id              = random_uuid.reference_writer_role.result
+  value                = "reference.writer"
 }
 
 resource "azuread_service_principal" "destiny_repository" {
@@ -39,7 +50,7 @@ resource "azuread_application_identifier_uri" "this" {
   identifier_uri = "api://${azuread_application_registration.destiny_repository.client_id}"
 }
 
-# Assign developers group to import access
+# Assign developers group all authentication scopes
 # This group is managed by click-ops in Entra Id
 resource "azuread_app_role_assignment" "developer_to_importer" {
   app_role_id         = azuread_application_app_role.importer.role_id
@@ -47,10 +58,14 @@ resource "azuread_app_role_assignment" "developer_to_importer" {
   resource_object_id  = azuread_service_principal.destiny_repository.object_id
 }
 
-# Assign developers group to referencer access
-# This group is managed by click-ops in Entra Id
-resource "azuread_app_role_assignment" "developer_to_referencer" {
-  app_role_id         = azuread_application_app_role.referencer.role_id
+resource "azuread_app_role_assignment" "developer_to_reference_reader" {
+  app_role_id         = azuread_application_app_role.reference_reader.role_id
+  principal_object_id = var.developers_group_id
+  resource_object_id  = azuread_service_principal.destiny_repository.object_id
+}
+
+resource "azuread_app_role_assignment" "developer_to_reference_writer" {
+  app_role_id         = azuread_application_app_role.reference_writer.role_id
   principal_object_id = var.developers_group_id
   resource_object_id  = azuread_service_principal.destiny_repository.object_id
 }
@@ -68,7 +83,8 @@ resource "azuread_application_api_access" "destiny_repository_auth" {
 
   role_ids = [
     azuread_application_app_role.importer.role_id,
-    azuread_application_app_role.referencer.role_id
+    azuread_application_app_role.reference_reader.role_id,
+    azuread_application_app_role.reference_writer.role_id
   ]
 }
 
