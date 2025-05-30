458d40d1a86bc6462ab330e022ea592cf60da91e
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-05-30 10:16:46 +1000

    reduced scope of healthcheck test

--- tests/routers/test_healthcheck.py
+++ tests/routers/test_healthcheck.py
@@ -52,7 +52,10 @@ async def test_healthcheck_success(app: FastAPI, client: AsyncClient) -> None:
         yield mock_session
 
     app.dependency_overrides[get_session] = mock_get_session
-    response = await client.get("/healthcheck/")
+    response = await client.get(
+        "/healthcheck/",
+        params={"database": True, "worker": False, "azure_blob_storage": False},
+    )
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"status": "ok"}
