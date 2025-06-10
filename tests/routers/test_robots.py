2206a495479f68698e3861eb48b15fcdcab9048b
Jack Walmisley <jack@covidence.org>
2025-06-11 10:13:56 +1200

    Added test to verify duplicate error thrown if robot name not unique

--- tests/routers/test_robots.py
+++ tests/routers/test_robots.py
@@ -7,8 +7,10 @@ from fastapi import FastAPI, status
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.exceptions import DuplicateError
 from app.domain.robots import routes as robots
 from app.domain.robots.sql import Robot as SQLRobot
+from app.main import duplicate_exception_handler
 
 
 @pytest.fixture
@@ -20,7 +22,7 @@ def app() -> FastAPI:
         FastAPI: FastAPI application instance.
 
     """
-    app = FastAPI()
+    app = FastAPI(exception_handlers={DuplicateError: duplicate_exception_handler})
 
     app.include_router(robots.router)
 
@@ -62,3 +64,22 @@ async def test_register_robot_happy_path(
     assert response.status_code == status.HTTP_201_CREATED
     data = await session.get(SQLRobot, response.json()["id"])
     assert data is not None
+
+
+async def test_add_robot_fails_when_name_is_the_same(
+    session: AsyncSession, client: AsyncClient
+) -> None:
+    """Test that registering a robot with the same name causes a 409 response."""
+    robot_in = {
+        "base_url": "http://www.mimetic-alloy.com",
+        "name": "T-1000",
+        "owner": "Skynet",
+        "description": "Liquid metal android assassin.",
+    }
+
+    existing_robot = SQLRobot(client_secret="secret-secret", **robot_in)
+    session.add(existing_robot)
+    await session.commit()
+
+    response = await client.post("/robot/", json=robot_in)
+    assert response.status_code == status.HTTP_409_CONFLICT
