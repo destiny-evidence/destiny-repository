7d08b01ddeb4d1111a505828a947c6b8e64d3cdc
Jack Walmisley <jack@covidence.org>
2025-06-10 12:54:36 +1200

    Adding robot models to the sdk

--- libs/sdk/src/destiny_sdk/robots.py
+++ libs/sdk/src/destiny_sdk/robots.py
@@ -287,3 +287,49 @@ If the URL expires, a new one can be generated using
         "individual reference. If there was an error with processing an individual "
         "reference, it is passed in the validation result file.",
     )
+
+
+class _RobotBase(BaseModel):
+    """
+    Base Robot class.
+
+    A Robot is a provider of enhancements to destiny repository
+    """
+
+    name: str = Field(description="The name of the robot, must be unique.")
+    base_url: HttpUrl = Field(
+        description="The base url of the robot. The robot must implement endpoints "
+        "base_url/single for the enhancement of single references and "
+        "base_url/batch for batch enhancements of references.",
+    )
+    description: str = Field(
+        description="Description of the enhancement the robot provides."
+    )
+    owner: str = Field(description="The owner/publisher of the robot.")
+
+
+class RobotIn(_RobotBase):
+    """The model for registering a new robot."""
+
+
+class Robot(_RobotBase):
+    """Then model for a registered robot."""
+
+    id: UUID4 = Field(
+        description="The id of the robot provided by destiny repository. "
+        "Used as the client_id when sending HMAC authenticated requests."
+    )
+
+
+class ProvisionedRobot(Robot):
+    """
+    The model for a provisioned robot.
+
+    Used only when a robot is initially created,
+    or when cycling a robot's client_secret.
+    """
+
+    client_secret: str = Field(
+        description="The client secret of the robot, used as the secret key "
+        "when sending HMAC authenticated requests."
+    )
--- tests/unit/sdk/test_robots.py
+++ tests/unit/sdk/test_robots.py
@@ -1,6 +1,7 @@
 import uuid
 
 import destiny_sdk
+from pydantic import HttpUrl
 
 
 def test_enhancement_request_valid():
@@ -20,3 +21,16 @@ def test_enhancement_request_valid():
     )
     assert enhancement_request.enhancement_parameters is None
     assert enhancement_request.error is None
+
+
+def test_provisioned_robot_valid():
+    provisioned_robot = destiny_sdk.robots.ProvisionedRobot(
+        id=uuid.uuid4(),
+        base_url=HttpUrl("https://www.domo-arigato-mr-robo.to"),
+        name="Mr. Roboto",
+        description="I have come to help you with your problems",
+        owner="Styx",
+        client_secret="secret, secret, I've got a secret",
+    )
+
+    assert provisioned_robot.owner == "Styx"
