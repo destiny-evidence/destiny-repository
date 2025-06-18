626436ee916c199636d6c010aead42faf3aaa360
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-06-18 17:01:42 +1000

    added config mapping

--- app/tasks.py
+++ app/tasks.py
@@ -12,6 +12,7 @@ settings = get_settings()
 broker: AsyncBroker = AzureServiceBusBroker(
     namespace=settings.message_broker_namespace,
     queue_name=settings.message_broker_queue_name,
+    max_lock_renewal_duration=settings.message_lock_renewal_duration,
 )
 
 if settings.env == Environment.LOCAL:
