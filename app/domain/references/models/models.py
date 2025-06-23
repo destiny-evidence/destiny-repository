6f71c34da698e5cf7c141f2962f63597606a56b4
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-06-23 14:03:50 +1000

    added serializability checker to file input constructor

--- app/domain/references/models/models.py
+++ app/domain/references/models/models.py
@@ -156,6 +156,7 @@ class Reference(
                 )
                 for enhancement in reference_in.enhancements or []
             ]
+            reference.check_serializability()
         except ValidationError as exception:
             raise SDKToDomainError(errors=exception.errors()) from exception
         else:
