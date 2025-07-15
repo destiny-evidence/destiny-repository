198197b115557137606267025167e8aff157b886
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-07-15 16:23:00 +1000

    updated import routers

--- app/domain/imports/routes.py
+++ app/domain/imports/routes.py
@@ -60,8 +60,22 @@ router = APIRouter(
     prefix="/imports", tags=["imports"], dependencies=[Depends(import_auth)]
 )
 
+import_record_router = APIRouter(
+    prefix="/records",
+    tags=["import-records"],
+    dependencies=[Depends(import_auth)],
+)
+router.include_router(import_record_router)
 
-@router.post("/record/", status_code=status.HTTP_201_CREATED)
+import_batch_router = APIRouter(
+    prefix="/{import_record_id}/batches",
+    tags=["import-batches"],
+    dependencies=[Depends(import_auth)],
+)
+import_record_router.include_router(import_batch_router)
+
+
+@import_record_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_record(
     import_record: destiny_sdk.imports.ImportRecordIn,
     import_service: Annotated[ImportService, Depends(import_service)],
@@ -73,9 +87,9 @@ async def create_record(
     return await record.to_sdk()
 
 
-@router.get("/record/{import_record_id}/")
+@import_record_router.get("/{import_record_id}/")
 async def get_record(
-    import_record_id: UUID4,
+    import_record_id: Annotated[UUID4, Path(title="The id of the import")],
     import_service: Annotated[ImportService, Depends(import_service)],
 ) -> destiny_sdk.imports.ImportRecordRead:
     """Get an import from the database."""
@@ -83,11 +97,11 @@ async def get_record(
     return await import_record.to_sdk()
 
 
-@router.patch(
-    "/record/{import_record_id}/finalise/", status_code=status.HTTP_204_NO_CONTENT
+@import_record_router.patch(
+    "/{import_record_id}/finalise/", status_code=status.HTTP_204_NO_CONTENT
 )
 async def finalise_record(
-    import_record_id: UUID4,
+    import_record_id: Annotated[UUID4, Path(title="The id of the import to finalise")],
     import_service: Annotated[ImportService, Depends(import_service)],
 ) -> None:
     """Finalise an import record."""
@@ -95,7 +109,7 @@ async def finalise_record(
     await import_service.finalise_record(import_record_id)
 
 
-@router.post("/record/{import_record_id}/batch/", status_code=status.HTTP_202_ACCEPTED)
+@import_batch_router.post("/", status_code=status.HTTP_202_ACCEPTED)
 async def enqueue_batch(
     import_record_id: Annotated[UUID4, Path(title="The id of the associated import")],
     batch: destiny_sdk.imports.ImportBatchIn,
@@ -113,7 +127,7 @@ async def enqueue_batch(
     return await import_batch.to_sdk()
 
 
-@router.get("/record/{import_record_id}/batch/")
+@import_batch_router.get("/")
 async def get_batches(
     import_record_id: Annotated[UUID4, Path(title="The id of the associated import")],
     import_service: Annotated[ImportService, Depends(import_service)],
@@ -125,19 +139,21 @@ async def get_batches(
     return [await batch.to_sdk() for batch in import_record.batches or []]
 
 
-@router.get("/batch/{import_batch_id}/")
+@import_batch_router.get("/{import_batch_id}/")
 async def get_batch(
+    import_record_id: Annotated[UUID4, Path(title="The id of the associated import")],
     import_batch_id: Annotated[UUID4, Path(title="The id of the import batch")],
     import_service: Annotated[ImportService, Depends(import_service)],
 ) -> destiny_sdk.imports.ImportBatchRead:
-    """Get batches associated to an import."""
+    """Get a particular batch."""
     import_batch = await import_service.get_import_batch(import_batch_id)
     return await import_batch.to_sdk()
 
 
-@router.get("/batch/{import_batch_id}/summary/")
+@import_batch_router.get("/{import_batch_id}/summary/")
 async def get_import_batch_summary(
-    import_batch_id: UUID4,
+    import_record_id: Annotated[UUID4, Path(title="The id of the associated import")],
+    import_batch_id: Annotated[UUID4, Path(title="The id of the import batch")],
     import_service: Annotated[ImportService, Depends(import_service)],
 ) -> destiny_sdk.imports.ImportBatchSummary:
     """Get a summary of an import batch's results."""
@@ -145,9 +161,10 @@ async def get_import_batch_summary(
     return await import_batch.to_sdk_summary()
 
 
-@router.get("/batch/{import_batch_id}/results/")
+@import_batch_router.get("/{import_batch_id}/results/")
 async def get_import_results(
-    import_batch_id: UUID4,
+    import_record_id: Annotated[UUID4, Path(title="The id of the associated import")],
+    import_batch_id: Annotated[UUID4, Path(title="The id of the import batch")],
     import_service: Annotated[ImportService, Depends(import_service)],
     result_status: ImportResultStatus | None = None,
 ) -> list[destiny_sdk.imports.ImportResultRead]:
