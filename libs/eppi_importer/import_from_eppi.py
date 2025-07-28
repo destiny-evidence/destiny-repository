f3224561cac87028b64238579e8d5e26411d6a9e
Daniel Ribeiro <djbreves@gmail.com>
2025-07-28 17:56:16 +1000

    Update checksum to match Azure blob

--- libs/eppi_importer/import_from_eppi.py
+++ libs/eppi_importer/import_from_eppi.py
@@ -1,6 +1,7 @@
 """Import references from a EPPI export file and write them to a .jsonl file."""
 
 import argparse
+import base64
 import hashlib
 import json
 from pathlib import Path
@@ -28,12 +29,15 @@ def main() -> None:
     args = arg_parser.parse_args()
 
     input_path = Path(args.input)
+
     with input_path.open("rb") as f:
         file_bytes = f.read()
-        checksum = hashlib.sha256(file_bytes).hexdigest()
+        checksum = base64.b64encode(hashlib.md5(file_bytes).digest()).decode("ascii")  # noqa: S324
     data = json.loads(file_bytes.decode("utf-8"))
     eppi_parser = EPPIParser(tags=args.tags)
-    references = eppi_parser.parse_data(data, source=f"eppi_data@{checksum}")
+    references = eppi_parser.parse_data(
+        data, source=f"eppi-reviewer-json-export@{checksum}"
+    )
 
     with Path(args.output).open("w") as f:
         for ref in references:
