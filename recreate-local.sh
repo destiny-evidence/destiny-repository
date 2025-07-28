ed555ce1832be54996bf5c56af2c9896ffc3d331
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-07-28 17:53:53 +1000

    reinstate signoz

--- recreate-local.sh
+++ recreate-local.sh
@@ -2,11 +2,11 @@
 
 # Convenience script to recreate the local environment.
 
-# COMPOSE_EXPERIMENTAL_GIT_REMOTE=1 docker compose -f docker-compose.signoz.yml down -v
+COMPOSE_EXPERIMENTAL_GIT_REMOTE=1 docker compose -f docker-compose.signoz.yml down -v
 
 docker compose --profile search --profile app down -v
 
-# COMPOSE_EXPERIMENTAL_GIT_REMOTE=1 docker compose -f docker-compose.signoz.yml up -d
+COMPOSE_EXPERIMENTAL_GIT_REMOTE=1 docker compose -f docker-compose.signoz.yml up -d
 
 docker compose --profile search --profile app up -d
 
