78b8acf748aa407068ce5005e8c1d81715b1ac22
Daniel Ribeiro <djbreves@gmail.com>
2025-07-28 16:57:28 +1000

    Update enhancements source

--- libs/eppi_importer/import_from_eppi.py
+++ libs/eppi_importer/import_from_eppi.py
@@ -1,6 +1,7 @@
 """Import references from a EPPI export file and write them to a .jsonl file."""
 
 import argparse
+import hashlib
 import json
 from pathlib import Path
 
@@ -26,10 +27,13 @@ def main() -> None:
     )
     args = arg_parser.parse_args()
 
-    with Path(args.input).open() as f:
-        data = json.load(f)
+    input_path = Path(args.input)
+    with input_path.open("rb") as f:
+        file_bytes = f.read()
+        checksum = hashlib.sha256(file_bytes).hexdigest()
+    data = json.loads(file_bytes.decode("utf-8"))
     eppi_parser = EPPIParser(tags=args.tags)
-    references = eppi_parser.parse_data(data)
+    references = eppi_parser.parse_data(data, source=f"eppi_data@{checksum}")
 
     with Path(args.output).open("w") as f:
         for ref in references:
