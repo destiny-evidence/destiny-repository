66e012df9a53b3d17f0dff9660a070ccdc322c1c
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-08-11 11:49:10 +1000

    Revert "trigger in all envs as we are now paying customers :)"
    This reverts commit 3c53792187891f1b4c0f4759a71f0c2e3660e4b2.

--- infra/app/honeycomb.tf
+++ infra/app/honeycomb.tf
@@ -57,6 +57,9 @@ resource "honeycombio_slack_recipient" "alerts" {
 }
 
 resource "honeycombio_trigger" "error_trigger" {
+  # Free tier only allows two triggers total, so we only create this in production.
+  count = var.environment == "production" ? 1 : 0
+
   name = "Application Error"
 
   query_id = honeycombio_query.application_errors.id
