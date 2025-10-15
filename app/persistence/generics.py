2afeae4169eb3a37c1cda535ccecbf0a8f06c207
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-10-16 10:25:57 +1100

    added vulture to pre-commit

--- app/persistence/generics.py
+++ app/persistence/generics.py
@@ -1,10 +1,11 @@
+# ruff: noqa: RUF100
 """Generic types for inheritance of persistance classes."""
 
 from typing import TYPE_CHECKING, TypeVar
 
 if TYPE_CHECKING:
     from app.domain.base import SQLAttributeMixin
-    from app.persistence.persistence import GenericPersistence
+    from app.persistence.persistence import GenericPersistence  # noqa: F401
 
 GenericPersistenceType = TypeVar("GenericPersistenceType", bound="GenericPersistence")
 GenericDomainModelType = TypeVar("GenericDomainModelType", bound="SQLAttributeMixin")
