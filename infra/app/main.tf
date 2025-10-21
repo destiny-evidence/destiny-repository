8e072876468a73950d8cc541dbf028fdec68f00f
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2025-10-21 12:15:32 +1100

    container app module v1.6.3->v1.7.1

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -143,7 +143,7 @@ data "azurerm_container_app" "this" {
 
 module "container_app" {
   source                          = "app.terraform.io/destiny-evidence/container-app/azure"
-  version                         = "1.6.3"
+  version                         = "1.7.1"
   app_name                        = var.app_name
   cpu                             = var.container_app_cpu
   environment                     = var.environment
@@ -210,7 +210,7 @@ module "container_app" {
 
 module "container_app_tasks" {
   source                          = "app.terraform.io/destiny-evidence/container-app/azure"
-  version                         = "1.6.3"
+  version                         = "1.7.1"
   app_name                        = "${var.app_name}-task"
   cpu                             = var.container_app_tasks_cpu
   environment                     = var.environment
