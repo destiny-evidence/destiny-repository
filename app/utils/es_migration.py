27150fff91990c24077e9f3d83a8f1db51b99cd4
Jack Walmisley <jack@covidence.org>
2025-10-22 14:48:24 +1300

    Allow specifying a target index when rolling back a single index

--- app/utils/es_migration.py
+++ app/utils/es_migration.py
@@ -18,45 +18,72 @@ INDICES = {
 }
 
 
-async def run_migration(indices: list[str], operation: str) -> None:
+async def run_migration(alias: str) -> None:
     """Run elasticsearch index migrations."""
     es_config = get_settings().es_config
 
     await es_manager.init(es_config)
 
     async with es_manager.client() as client:
-        for index_name in indices:
-            document_class = INDICES[index_name]
-            manager = IndexManager(document_class, index_name, client)
-            if operation == "migrate":
-                # If the index does not exist, migrating will create it.
-                await manager.migrate(delete_old=False)
-            elif operation == "rollback":
-                await manager.rollback()
+        document_class = INDICES[alias]
+        manager = IndexManager(document_class, alias, client)
+
+        await manager.migrate(delete_old=False)
+
+    await es_manager.close()
+
+
+async def run_rollback(alias: str, target_index: str | None = None) -> None:
+    """Roll back to the previous index, or the target."""
+    es_config = get_settings().es_config
+
+    await es_manager.init(es_config)
+
+    async with es_manager.client() as client:
+        document_class = INDICES[alias]
+        manager = IndexManager(document_class, alias, client)
+
+        await manager.rollback(target_index=target_index)
 
     await es_manager.close()
 
 
 def argument_parser() -> argparse.ArgumentParser:
     """Create argument parser for migrating indicies."""
-    parser = argparse.ArgumentParser(description="Migrate elasticsearch indicies.")
+    parser = argparse.ArgumentParser(description="Migrate or roll back an ES index.")
 
     parser.add_argument(
         "-i",
         "--index",
         type=str,
         choices=[*INDICES, "all"],
-        help="Name of the index to mgirate, or 'all' to migrate all indices.",
+        help="Name of the index.",
         required=True,
     )
 
     parser.add_argument(
-        "-o",
-        "--operation",
+        "-m",
+        "--migrate",
+        action="store_true",
+        help="Migrate the index",
+        required=False,
+    )
+
+    parser.add_argument(
+        "-r",
+        "--rollback",
+        action="store_true",
+        help="Rollback the index",
+        required=False,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--target-index",
         type=str,
-        choices=["migrate", "rollback"],
-        help="Whether to migrate the index forward or roll back to a previous index.",
-        required=True,
+        help="Optional param to roll back to a target index name.",
+        required=False,
+        default=None,
     )
 
     return parser
@@ -68,4 +95,14 @@ if __name__ == "__main__":
 
     indices = [*INDICES] if args.index == "all" else [args.index]
 
-    asyncio.run(run_migration(indices=indices, operation=args.operation))
+    if len(indices) > 1 and args.target_index:
+        msg = "Can only specify target_index when rolling back a single index."
+        raise RuntimeError(msg)
+
+    if args.migrate:
+        for index in indices:
+            asyncio.run(run_migration(alias=index))
+
+    elif args.rollback:
+        for index in indices:
+            asyncio.run(run_rollback(alias=index, target_index=args.target_index))
