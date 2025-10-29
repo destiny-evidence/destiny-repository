ebc1283b294559a906f239acd5544404826cbd80
Jack Walmisley <jack@covidence.org>
2025-10-29 13:18:49 +1300

    Add external directory provider

--- infra/app/terraform.tf
+++ infra/app/terraform.tf
@@ -50,6 +50,12 @@ provider "azurerm" {
 provider "azuread" {
 }
 
+provider "azuread" {
+  tenant_id = var.external_directory_tenant_id
+  client_id = var.external_directory_client_id
+  alias     = "external_directory"
+}
+
 provider "github" {
   owner = "destiny-evidence"
   app_auth {
