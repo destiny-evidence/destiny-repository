0c2c04f01c9dc1210e2fa01f6d96d92e05aebc17
Tim Repke <repke@mcc-berlin.net>
2025-11-07 16:10:08 +0100

    Add tests to Reference convenience methods; Move additional experimental methods to labs module

---
+++ libs/sdk/src/destiny_sdk/labs/references.py
@@ -0,0 +1,135 @@
+"""Extended Reference class for the Destiny SDK with added experimental convenience methods and properties."""
+
+from collections.abc import Generator
+
+from pydantic import TypeAdapter
+
+from destiny_sdk.references import Reference as BaseReference
+from destiny_sdk.enhancements import (
+    Annotation,
+    AnnotationType,
+    BibliographicMetadataEnhancement,
+    EnhancementType,
+)
+from destiny_sdk.identifiers import ExternalIdentifier, ExternalIdentifierType
+
+external_identifier_adapter = TypeAdapter(ExternalIdentifier)
+
+
+class Reference(BaseReference):
+    def _get_id(self, identifier_type: ExternalIdentifierType) -> str | int | None:
+        """Fetch an identifier matching the given identifier_type."""
+        for identifier in self.identifiers or []:
+            if identifier.identifier_type == identifier_type:
+                return identifier.identifier
+        return None
+
+    @property
+    def openalex_id(self) -> str | None:
+        """Return an OpenAlex ID for the reference."""
+        return self._get_id(identifier_type=ExternalIdentifierType.OPEN_ALEX)
+
+    @property
+    def doi(self) -> str | None:
+        """Return a DOI for the reference."""
+        return self._get_id(identifier_type=ExternalIdentifierType.DOI)
+
+    @property
+    def pubmed_id(self) -> int | None:
+        """Return a pubmed ID for the reference."""
+        return self._get_id(identifier_type=ExternalIdentifierType.PM_ID)
+
+    @property
+    def abstract(self) -> str | None:
+        """Return an abstract for the reference."""
+        for enhancement in self.enhancements or []:
+            if enhancement.content.enhancement_type == EnhancementType.ABSTRACT:
+                return enhancement.content.abstract
+        return None
+
+    @property
+    def publication_year(self) -> int | None:
+        """Return a publication year for the reference."""
+        for meta in self.it_bibliographics():
+            if meta.publication_year is not None:
+                return meta.publication_year
+        return None
+
+    @property
+    def title(self) -> str | None:
+        """The title of the reference. If multiple are present, return first one."""
+        for meta in self.it_bibliographics():
+            if meta.title is not None:
+                return meta.title
+        return None
+
+    def it_bibliographics(self) -> Generator[BibliographicMetadataEnhancement, None, None]:
+        """Iterate bibliographic enhancements."""
+        for enhancement in self.enhancements or []:
+            if enhancement.content.enhancement_type == EnhancementType.BIBLIOGRAPHIC:
+                yield enhancement.content
+
+    def it_annotations(
+            self,
+            source: str | None = None,
+            annotation_type: AnnotationType | None = None,
+            scheme: str | None = None,
+            label: str | None = None,
+    ) -> Generator[Annotation, None, None]:
+        """
+        Iterate annotation enhancements for the given filters.
+
+        :param source: Optional filter for Enhancement.source
+        :param annotation_type: Optional filter for
+                                AnnotationEnhancement.annotation_type
+        :param scheme: Optional filter for Annotation.scheme
+        :param label: Optional filter for Annotation.label
+        """
+        for enhancement in self.enhancements or []:
+            if enhancement.content.enhancement_type == EnhancementType.ANNOTATION:
+                if source is not None and enhancement.source != source:
+                    continue
+                for annotation in enhancement.content.annotations:
+                    if (
+                            annotation_type is not None
+                            and annotation.annotation_type != annotation_type
+                    ):
+                        continue
+                    if scheme is not None and annotation.scheme != scheme:
+                        continue
+                    if label is not None and annotation.label != label:
+                        continue
+                    yield annotation
+
+    def has_bool_annotation(
+            self,
+            source: str | None = None,
+            scheme: str | None = None,
+            label: str | None = None,
+            expected_value: bool = True,
+    ) -> bool | None:
+        """
+        Check if a specific annotation exists and is true.
+
+        :param source: Optional filter for Enhancement.source
+        :param scheme: Optional filter for Annotation.scheme
+        :param label: Optional filter for Annotation.label
+        :param expected_value: Specify whether boolean annotation should be to be true or false
+        :return: Returns the boolean value for the first annotation matching the filters
+                 or None if nothing is found.
+        """
+        if scheme is None and label is None:
+            raise AssertionError('Please provide at least one of the optional scheme or label filters.')
+
+        found_annotation = False
+        for annotation in self.it_annotations(
+                source=source,
+                annotation_type=AnnotationType.BOOLEAN,
+                scheme=scheme,
+                label=label,
+        ):
+            if annotation.value == expected_value:
+                return True
+            else:
+                found_annotation = True
+        return False if found_annotation else None
--- libs/sdk/src/destiny_sdk/references.py
+++ libs/sdk/src/destiny_sdk/references.py
@@ -1,20 +1,12 @@
 """Reference classes for the Destiny SDK."""
 
-from collections.abc import Generator
 from typing import Self
 
 from pydantic import UUID4, BaseModel, Field, TypeAdapter
 
 from destiny_sdk.core import _JsonlFileInputMixIn
-from destiny_sdk.enhancements import (
-    Annotation,
-    AnnotationType,
-    BibliographicMetadataEnhancement,
-    Enhancement,
-    EnhancementFileInput,
-    EnhancementType,
-)
-from destiny_sdk.identifiers import ExternalIdentifier, ExternalIdentifierType
+from destiny_sdk.enhancements import Enhancement, EnhancementFileInput
+from destiny_sdk.identifiers import ExternalIdentifier
 from destiny_sdk.visibility import Visibility
 
 external_identifier_adapter = TypeAdapter(ExternalIdentifier)
@@ -56,114 +48,6 @@ class Reference(_JsonlFileInputMixIn, BaseModel):
                 for enhancement in es_reference["_source"].get("enhancements", [])
             ],
         )
-
-    def _get_id(self, kind: ExternalIdentifierType) -> str | None:
-        """Fetch first identifier enhancements."""
-        for identifier in self.identifiers or []:
-            if identifier.kind == kind:
-                return identifier.identifier
-        return None
-
-    @property
-    def openalex_id(self) -> str | None:
-        """The OpenAlex ID of the reference; returns first."""
-        return self._get_id(kind=ExternalIdentifierType.OPEN_ALEX)
-
-    @property
-    def doi(self) -> str | None:
-        """The DOI of the reference; returns first."""
-        return self._get_id(kind=ExternalIdentifierType.DOI)
-
-    @property
-    def pubmed_id(self) -> str | None:
-        """The pubmed ID of the reference; returns first."""
-        return self._get_id(kind=ExternalIdentifierType.PM_ID)
-
-    @property
-    def abstract(self) -> str | None:
-        """The abstract of the reference; returns first."""
-        for enhancement in self.enhancements or []:
-            if enhancement.content.enhancement_type == EnhancementType.ABSTRACT:
-                return enhancement.content.abstract
-        return None
-
-    @property
-    def publication_year(self) -> int | None:
-        """The publication year of the reference; returns first."""
-        for meta in self.bibliographics():
-            if meta.publication_year is not None:
-                return meta.publication_year
-        return None
-
-    @property
-    def title(self) -> str | None:
-        """The title of the reference. If multiple are present, return first one."""
-        for meta in self.bibliographics():
-            if meta.title is not None:
-                return meta.title
-        return None
-
-    def bibliographics(self) -> Generator[BibliographicMetadataEnhancement, None, None]:
-        """List bibliographic metadata enhancements."""
-        for enhancement in self.enhancements or []:
-            if enhancement.content.enhancement_type == EnhancementType.BIBLIOGRAPHIC:
-                yield enhancement.content
-
-    def annotations(
-        self,
-        source: str | None = None,
-        annotation_type: AnnotationType | None = None,
-        scheme: str | None = None,
-        label: str | None = None,
-    ) -> Generator[Annotation, None, None]:
-        """
-        Generate a list of annotations for the given filters.
-
-        :param source: Optional filter for Enhancement.source
-        :param annotation_type: Optional filter for
-                                AnnotationEnhancement.annotation_type
-        :param scheme: Optional filter for Annotation.scheme
-        :param label: Optional filter for Annotation.label
-        """
-        for enhancement in self.enhancements or []:
-            if enhancement.content.enhancement_type == EnhancementType.ANNOTATION:
-                if source is not None and enhancement.source != source:
-                    continue
-                for annotation in enhancement.content.annotations:
-                    if (
-                        annotation_type is not None
-                        and annotation.annotation_type != annotation_type
-                    ):
-                        continue
-                    if scheme is not None and annotation.scheme != scheme:
-                        continue
-                    if label is not None and annotation.label != label:
-                        continue
-                    yield annotation
-
-    def is_true(
-        self,
-        source: str | None = None,
-        scheme: str | None = None,
-        label: str | None = None,
-    ) -> bool | None:
-        """
-        Check if a specific annotation exists and is true.
-
-        :param source: Optional filter for Enhancement.source
-        :param scheme: Optional filter for Annotation.scheme
-        :param label: Optional filter for Annotation.label
-        :return: Returns the boolean value for the first annotation matching the filters
-                 or None if nothing is found.
-        """
-        for annotation in self.annotations(
-            source=source,
-            annotation_type=AnnotationType.BOOLEAN,
-            scheme=scheme,
-            label=label,
-        ):
-            return annotation.value
-        return None
 
 
 class ReferenceFileInput(_JsonlFileInputMixIn, BaseModel):
