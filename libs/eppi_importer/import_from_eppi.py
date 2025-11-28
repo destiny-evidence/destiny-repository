fb7392b1d475f13d2126785aa956b0d03d2b3027
Jack Walmisley <jack@covidence.org>
2025-11-28 15:49:16 +1300

    Added arguments to the parser to facilitate raw enhancement creation

--- libs/eppi_importer/import_from_eppi.py
+++ libs/eppi_importer/import_from_eppi.py
@@ -4,6 +4,7 @@ import argparse
 import base64
 import hashlib
 import json
+from datetime import UTC, datetime
 from pathlib import Path
 
 from destiny_sdk.parsers.eppi_parser import EPPIParser
@@ -32,6 +33,34 @@ def main() -> None:
         required=True,
         help="Source identifier for provenance (e.g., alive-hpv-partnership)",
     )
+
+    arg_parser.add_argument(
+        "--include-raw",
+        action="store_true",
+        help="Whether to include raw data enhancements for the import",
+    )
+
+    arg_parser.add_argument(
+        "--source-export-timestamp",
+        type=int,
+        default=None,
+        help="Timestamp of when the source file was exported",
+    )
+
+    arg_parser.add_argument(
+        "--description",
+        type=str,
+        default=None,
+        help="Description of the data to be stored as a raw enhancement",
+    )
+
+    arg_parser.add_argument(
+        "--codeset-id",
+        type=int,
+        default=None,
+        help="The codeset id of the attributes on the incoming references.",
+    )
+
     args = arg_parser.parse_args()
 
     input_path = Path(args.input)
@@ -39,8 +68,25 @@ def main() -> None:
     with input_path.open("rb") as f:
         file_bytes = f.read()
         checksum = base64.b64encode(hashlib.md5(file_bytes).digest()).decode("ascii")  # noqa: S324
+
     data = json.loads(file_bytes.decode("utf-8"))
-    eppi_parser = EPPIParser(tags=args.tags)
+
+    source_export_date = (
+        datetime.fromtimestamp(args.source_export_timestamp, tz=UTC)
+        if args.source_export_timestamp
+        else None
+    )
+
+    metadata = {"codeset_id": args.codeset_id} if args.codeset_id else None
+
+    eppi_parser = EPPIParser(
+        tags=args.tags,
+        include_raw_data=args.include_raw,
+        source_export_date=source_export_date,
+        data_description=args.description,
+        raw_enhancement_metadata=metadata,
+    )
+
     references = eppi_parser.parse_data(
         data,
         source=args.source,
@@ -48,8 +94,7 @@ def main() -> None:
     )
 
     with Path(args.output).open("w") as f:
-        for ref in references:
-            f.write(ref.to_jsonl() + "\n")
+        f.writelines(ref.to_jsonl() + "\n" for ref in references)
 
 
 if __name__ == "__main__":
