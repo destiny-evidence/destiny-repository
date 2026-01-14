7115ec8ed25ea37297d8cfb8203121e7da3149ff
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2026-01-14 13:36:36 +1100

    upgrade psql size

--- infra/app/locals.tf
+++ infra/app/locals.tf
@@ -10,13 +10,13 @@ locals {
   is_production  = var.environment == "production"
   is_development = var.environment == "development"
 
-  prod_db_storage_mb = 65536
+  prod_db_storage_mb = 4194304
   dev_db_storage_mb  = 32768
 
   # IPOS tiers for postgresql flexible server
   # We use the default for our storage size as defined at
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/postgresql_flexible_server#storage_tier-defaults-based-on-storage_mb
-  prod_db_storage_tier = "P6"
+  prod_db_storage_tier = "P50"
   dev_db_storage_tier  = "P4"
 
   minimum_resource_tags = {
