46afefcd8cdc308f3d5aad1ec51d5b3f8d179e84
Jack Walmisley <jack@covidence.org>
2026-01-15 12:19:16 +1300

    Revert honeycomb environment to resource

--- infra/app/honeycomb.tf
+++ infra/app/honeycomb.tf
@@ -1,15 +1,10 @@
-# Were getting duplicates here with needing the configuration key to exist already.
-# Is likely an issue with provider/resource evolution.
-data "honeycombio_environment" "this" {
-    detail_filter {
-      name  = "name"
-      value = var.environment
-    }
+resource "honeycombio_environment" "this" {
+  name = var.environment
 }
 
 resource "honeycombio_api_key" "this" {
   name           = "${var.app_name}-${var.environment}-ingest-api-key"
-  environment_id = data.honeycombio_environment.this.id
+  environment_id = honeycombio_environment.this.id
   type           = "ingest"
 
   permissions {
