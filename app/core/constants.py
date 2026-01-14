35d5fdad6d2fdf6e3d343b1ac246f91629aa06d1
jack-o-the-green <132786979+jack-o-the-green@users.noreply.github.com>
2026-01-15 09:27:38 +1300

    Fix typo in app/core/constants.py
    Co-authored-by: Adam Hamilton <43816242+Adam-Hammo@users.noreply.github.com>

--- app/core/constants.py
+++ app/core/constants.py
@@ -4,6 +4,6 @@ from typing import Literal
 
 # The maximum depth to which reference duplicates are propagated. A depth
 # of 2, means only direct duplicates are allowed. Any increases to this
-# constant will cause significat increases in performance cost and
+# constant will cause significant increases in performance cost and
 # data model complexity.
 MAX_REFERENCE_DUPLICATE_DEPTH: Literal[2] = 2
