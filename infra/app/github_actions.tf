1c2f8e67b9dec7e1f74cfafdca3743d19628fa1b
Jack Walmisley <jack@covidence.org>
2026-05-14 12:34:26 +1200

    Remove external directory authentication infra

--- infra/app/github_actions.tf
+++ infra/app/github_actions.tf
@@ -48,14 +48,14 @@ resource "github_actions_environment_variable" "azure_directory_client_id" {
   environment   = github_repository_environment.environment.environment
   repository    = github_repository_environment.environment.repository
   variable_name = "AZURE_DIRECTORY_CLIENT_ID"
-  value         = var.external_directory_enabled ? azuread_application_registration.external_directory_github_actions.client_id : azuread_application_registration.github_actions.client_id
+  value         = azuread_application_registration.github_actions.client_id
 }
 
 resource "github_actions_environment_variable" "azure_directory_tenant_id" {
   environment   = github_repository_environment.environment.environment
   repository    = github_repository_environment.environment.repository
   variable_name = "AZURE_DIRECTORY_TENANT_ID"
-  value         = var.external_directory_enabled ? var.external_directory_tenant_id : var.azure_tenant_id
+  value         = var.azure_tenant_id
 }
 
 resource "github_actions_environment_variable" "azure_login_url" {
@@ -122,7 +122,7 @@ resource "github_actions_environment_variable" "destiny_api_identifier_uri" {
   repository    = github_repository_environment.environment.repository
   environment   = github_repository_environment.environment.environment
   variable_name = "DESTINY_API_IDENTIFIER_URI"
-  value         = var.external_directory_enabled ? azuread_application_identifier_uri.external_directory_identifier_uri.identifier_uri : azuread_application_identifier_uri.this.identifier_uri
+  value         = azuread_application_identifier_uri.this.identifier_uri
 }
 
 resource "github_actions_environment_variable" "pypi_repository" {
