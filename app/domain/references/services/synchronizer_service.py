8ed92c97550751fe64c7b674104e925243d0949e
Adam-Hammo <43816242+Adam-Hammo@users.noreply.github.com>
2026-06-03 11:13:41 +1000

    add second pass to reindex duplicates in bulk sync

--- app/domain/references/services/synchronizer_service.py
+++ app/domain/references/services/synchronizer_service.py
@@ -116,17 +116,37 @@ class ReferenceSynchronizer(GenericSynchronizer[Reference]):
             AsyncGenerator[ReferenceSearchProjection, None]
         ):
             """Generate references for indexing."""
-            for reference_id_chunk in list_chunker(
-                ids,
-                chunk_size,
-            ):
+            indexed_ids: set[UUID] = set()
+            redirect_ids: set[UUID] = set()
+
+            for reference_id_chunk in list_chunker(ids, chunk_size):
                 references = await self.sql_uow.references.get_by_pks(
                     reference_id_chunk,
                     preload=self._required_preloads,
                 )
                 for reference in references:
                     if reference.is_canonical_like:
+                        indexed_ids.add(reference.id)
                         yield await self._to_indexable(reference)
+                    elif reference.duplicate_decision and (
+                        canonical_id := (
+                            reference.duplicate_decision.canonical_reference_id
+                        )
+                    ):
+                        redirect_ids.add(canonical_id)
+
+            # Re-index the canonicals of any duplicates we saw, skipping any that were
+            # already indexed directly in the pass above.
+            for canonical_id_chunk in list_chunker(
+                list(redirect_ids - indexed_ids), chunk_size
+            ):
+                canonicals = await self.sql_uow.references.get_by_pks(
+                    canonical_id_chunk,
+                    preload=self._required_preloads,
+                )
+                for canonical in canonicals:
+                    if canonical.is_canonical_like:
+                        yield await self._to_indexable(canonical)
 
         return await self.es_uow.references.add_bulk(reference_generator())
 
